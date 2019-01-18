226de4ffbc8b5c912d889fdd7b4ac835262920c8
Miracle <120007700@qq.com>
2019-01-18 13:02:44 +0800

    司机端登录 Bug

--- app/Http/Controllers/Driver/AuthorizationsController.php
+++ app/Http/Controllers/Driver/AuthorizationsController.php
@@ -4,18 +4,17 @@ namespace App\Http\Controllers\Driver;
 
 
 use App\Http\Requests\Driver\AuthorizationRequest;
+use App\Models\Driver;
 use App\Models\DriverEquipment;
 use App\Models\DriverSocketToken;
 use Illuminate\Support\Facades\Auth;
 
 class AuthorizationsController extends Controller
 {
-    public $driver_ttl = 480;  //司机端token有效时间 单位:(分钟)
-
     public function store(AuthorizationRequest $request)
     {
         $equ = DriverEquipment::where('imei', $request->imei)->first();
-        $driver = $equ->driver;
+        $driver = Driver::find($equ->driver_id);
 
         $driver->update([
             'name' => $equ->name,
@@ -31,7 +30,7 @@ class AuthorizationsController extends Controller
         $driver_token = new DriverSocketToken();
         $driver_token->token = $token;
         $driver_token->driver()->associate($driver);
-        $driver_token->expired_at = now()->addMinutes($this->driver_ttl);
+        $driver_token->expired_at = now()->addMinutes(Auth::guard('driver')->factory()->getTTL());
         $driver_token->save();
 
         return $this->respondWithToken($token)->setStatusCode(201);
@@ -43,7 +42,7 @@ class AuthorizationsController extends Controller
         return $this->response->array([
             'access_token' => $token,
             'token_type' => 'Bearer',
-            'expires_in' => $this->driver_ttl * 60 // token有效的时间(单位:秒)
+            'expires_in' => Auth::guard('driver')->factory()->getTTL() * 60 // token有效的时间(单位:秒)
         ]);
     }
 
